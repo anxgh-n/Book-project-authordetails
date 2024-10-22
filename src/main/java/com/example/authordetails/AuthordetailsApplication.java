468f58b529d5d24706abb602e83207fc4c69fe49
anxgh-n <anaghmanoj@gmail.com>
2024-10-22 14:45:50 +0530

    new author

---
+++ src/main/java/com/example/authordetails/AuthordetailsApplication.java
@@ -0,0 +1,13 @@
+package com.example.authordetails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthordetailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthordetailsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/authordetails/AuthordetailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.authordetails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthordetailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
